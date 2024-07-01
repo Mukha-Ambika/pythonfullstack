5e9b6e0f94bd0d32be7507af8e97e83ea1804a0d
Abhijith14 <abhijithukzm@gmail.com>
2024-07-01 18:44:45 +0100

    test code update

--- app/main.py
+++ app/main.py
@@ -4,16 +4,18 @@ from mangum import Mangum
 
 app = FastAPI()
 
-app.include_router(auth.router)
+# Lambda handler
+handler = Mangum(app)
+
+# app.include_router(auth.router)
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to Sharbo!"}
 
-# Lambda handler
-handler = Mangum(app)
+
 
 # This allows the handler to work as expected when run locally
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=80)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=80)
