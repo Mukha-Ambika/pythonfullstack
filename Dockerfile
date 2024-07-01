119c2dae3200d709fc29f26aac4b7a78e951b3ad
Abhijith14 <abhijithukzm@gmail.com>
2024-07-01 21:28:23 +0100

    test commit

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,13 @@ FROM public.ecr.aws/lambda/python:3.9 as lambda
 
 COPY requirements.txt .
 
-RUN pip install requests pandas beautifulsoup4 lxml
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY ./* ./sharbo_app/
+# COPY ./* ./sharbo_app/
 
-COPY my_lambda_function.py ./
+COPY app /app
+WORKDIR /app
+
+# COPY my_lambda_function.py ./
 
 CMD [ "my_lambda_function.handler" ]
--- app/my_lambda_function.py
+++ app/my_lambda_function.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI, HTTPException
 import requests
-from bs4 import BeautifulSoup
-import pandas as pd
 from mangum import Mangum
 
-from sharbo_app.app.utils.get_env import *
+# from app.utils.get_env import *
 
 app = FastAPI()
 # Lambda handler
@@ -15,7 +13,7 @@ def scrape_wikipedia():
     try:
         return {
             'statusCode': 200,
-            'body': CLERK_API_BASE_URL
+            'body': 'CLERK_API_BASE_URL'
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
